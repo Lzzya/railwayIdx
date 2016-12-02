befd39300272d2832327085910d97f43d4690f3d
Xiaochun Lu <xclu@bjtu.edu.cn>
2016-12-03 00:26:15 +0800

    12-3
    替换了时间代码。

--- RailIndexPrj/server.R
+++ RailIndexPrj/server.R
@@ -1268,10 +1268,10 @@ shinyServer(function(input, output) {
   #mileage_delta-----新增营业里程（陈雯）construct_investment----基本建设投资
   #df_yearly<-read.csv("营业里程.csv",head=T)
   a<-length(df_yearly$construct_investment)
-  tm1<-df_yearly$tm[2:a]
+  tm4<-df_yearly$tm[2:a]
   construct_investment1<-df_yearly$construct_investment[2:a]
   mileage_delta<-round(df_yearly$mileage_delta[2:a],2)
-  df_yearly1<-data.frame(tm1,construct_investment1,mileage_delta)
+  df_yearly1<-data.frame(tm4,construct_investment1,mileage_delta)
   
   mileage_olsRegModel<-lm(construct_investment1~mileage_delta,data=df_yearly1)
   df_yearly1$linearRegPred<-as.integer(predict(mileage_olsRegModel,newdata=df_yearly1))
@@ -1279,7 +1279,7 @@ shinyServer(function(input, output) {
   df_yearly1$frRegPred<-as.integer(predict(mileage_rfRegModel,df_yearly1))
   mileage_svmRegModel<-svm(construct_investment1~mileage_delta,data=df_yearly1,type="eps-regression",cross=dim(df_yearly1)[1]/2)
   df_yearly1$svmRegPred<-as.integer(predict(mileage_svmRegModel,df_yearly1))
-  mileage_len<-length(df_yearly1$tm1)
+  mileage_len<-length(df_yearly1$tm4)
   
   
   plotCurve<-function(db,xdata,ydata)
@@ -1293,40 +1293,40 @@ shinyServer(function(input, output) {
     if(input$mileage_year_start> input$mileage_year_end)  {
       
       if (input$mileage_stat_data) {
-        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm1,df_yearly1$construct_investment1)
+        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm4,df_yearly1$construct_investment1)
       }
       else
       {
-        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm1,df_yearly1$linearRegPred)
+        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm4,df_yearly1$linearRegPred)
       }
     }
     else{
-      df_yearly1sub<-subset(df_yearly1,substr(df_yearly1$tm1,1,4)>=input$mileage_year_start) 
-      df_yearly1sub<-subset(df_yearly1sub,substr(df_yearly1sub$tm1,1,4)<=input$mileage_year_end)
+      df_yearly1sub<-subset(df_yearly1,substr(df_yearly1$tm4,1,4)>=input$mileage_year_start) 
+      df_yearly1sub<-subset(df_yearly1sub,substr(df_yearly1sub$tm4,1,4)<=input$mileage_year_end)
       if (input$mileage_stat_data) {
-        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm1,df_yearly1sub$construct_investment1)
+        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm4,df_yearly1sub$construct_investment1)
       }
       else
       {
-        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm1,df_yearly1sub$linearRegPred)
+        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm4,df_yearly1sub$linearRegPred)
       }
     }
     if(input$mileage_predict_data){
       
-      mileage_p<-mileage_p+geom_line(aes(x=tm1,y=linearRegPred),color="blue",size=0.8)+geom_point(aes(x=tm1,y=linearRegPred),fill='cornsilk',size=4,shape=21,colour="darkblue",position=position_dodge(width=0.2))
+      mileage_p<-mileage_p+geom_line(aes(x=tm4,y=linearRegPred),color="blue",size=0.8)+geom_point(aes(x=tm4,y=linearRegPred),fill='cornsilk',size=4,shape=21,colour="darkblue",position=position_dodge(width=0.2))
     }
     
     if (input$mileage_stat_data) {
-      mileage_p<-mileage_p+geom_point(aes(x=tm1,y=construct_investment1),color="red",size=3,shape=21)
+      mileage_p<-mileage_p+geom_point(aes(x=tm4,y=construct_investment1),color="red",size=3,shape=21)
     }
     mileage_p+ylab("固定资产值")+xlab("时间")+geom_point(shape=21,color='red',fill='cornsilk',size=3)
   })
   output$mileage_fixed_assets_investment_output<-renderText({
     mileage_x<-as.numeric(input$mileage_input)
     mileage_delta<-c(mileage_x)
-    tm1<-c(2016)
+    tm4<-c(2016)
     construct_investment1<-c(0)
-    inputdata<-data.frame(tm1,construct_investment1,mileage_delta)
+    inputdata<-data.frame(tm4,construct_investment1,mileage_delta)
     mileage_pred<-as.integer(predict(mileage_olsRegModel,inputdata,interval="prediction",level=0.95))
     paste("多元回归预测：",mileage_pred[1],"预测区间95%：(",mileage_pred[2],",",mileage_pred[3],")" ) 
   }
@@ -1336,9 +1336,9 @@ shinyServer(function(input, output) {
   output$mileage_fixed_assets_investment_FRR<-renderText({
     mileage_x<-as.numeric(input$mileage_input)
     mileage_delta<-c(mileage_x)
-    tm1<-c(2016)
+    tm4<-c(2016)
     construct_investment1<-c(0)
-    inputdata<-data.frame(tm1,construct_investment1,mileage_delta)
+    inputdata<-data.frame(tm4,construct_investment1,mileage_delta)
     railfixed_assets_investment<-predict(mileage_rfRegModel,inputdata)   #rfRegModel随机森林在最初已经计算得到
     paste("随机森林回归预测：",as.integer(railfixed_assets_investment[1])  ) 
     
@@ -1349,9 +1349,9 @@ shinyServer(function(input, output) {
   output$mileage_fixed_assets_investment_zhi<-renderText({
     mileage_x<-as.numeric(input$mileage_input)
     mileage_delta<-c(mileage_x)
-    tm1<-c(2016)
+    tm4<-c(2016)
     construct_investment1<-c(0)
-    inputdata<-data.frame(tm1,construct_investment1,mileage_delta)
+    inputdata<-data.frame(tm4,construct_investment1,mileage_delta)
     mileage_pred<-as.integer(predict(mileage_svmRegModel,inputdata))
     
     paste("支持向量机预测：",mileage_pred)
@@ -1364,31 +1364,31 @@ shinyServer(function(input, output) {
     if(input$mileage_year_start> input$mileage_year_end)  {
       
       if (input$mileage_stat_data) {
-        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm1,df_yearly1$construct_investment1)
+        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm4,df_yearly1$construct_investment1)
       }
       else
       {
-        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm1,df_yearly1$frRegPred)
+        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm4,df_yearly1$frRegPred)
       }
     }
     else{
-      df_yearly1sub<-subset(df_yearly1,substr(df_yearly1$tm1,1,4)>=input$mileage_year_start) 
-      df_yearly1sub<-subset(df_yearly1sub,substr(df_yearly1$tm1,1,4)<=input$mileage_year_end)
+      df_yearly1sub<-subset(df_yearly1,substr(df_yearly1$tm4,1,4)>=input$mileage_year_start) 
+      df_yearly1sub<-subset(df_yearly1sub,substr(df_yearly1$tm4,1,4)<=input$mileage_year_end)
       if (input$mileage_stat_data) {
-        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm1,df_yearly1sub$construct_investment1)
+        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm4,df_yearly1sub$construct_investment1)
       }
       else
       {
-        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm1,df_yearly1sub$frRegPred)
+        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm4,df_yearly1sub$frRegPred)
       }
     }
     
     if(input$mileage_predict_data){
-      mileage_p<-mileage_p+geom_line(aes(x=tm1,y=frRegPred),color="blue",size=0.8,show.legend = T)+geom_point(aes(x=tm1,y=frRegPred),fill='cornsilk',size=4,shape=21,colour="darkblue",position=position_dodge(width=0.2))
+      mileage_p<-mileage_p+geom_line(aes(x=tm4,y=frRegPred),color="blue",size=0.8,show.legend = T)+geom_point(aes(x=tm4,y=frRegPred),fill='cornsilk',size=4,shape=21,colour="darkblue",position=position_dodge(width=0.2))
     }
     
     if (input$mileage_stat_data) {
-      mileage_p<-mileage_p+geom_point(aes(x=tm1,y=construct_investment1),color="red",size=3,shape=21)
+      mileage_p<-mileage_p+geom_point(aes(x=tm4,y=construct_investment1),color="red",size=3,shape=21)
     }
     mileage_p+ylab("固定资产值")+xlab("时间")+geom_point(shape=21,color='red',fill='cornsilk',size=3)
   })
@@ -1399,33 +1399,33 @@ shinyServer(function(input, output) {
     if(input$mileage_year_start> input$mileage_year_end)  {
       
       if (input$mileage_stat_data) {
-        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm1,df_yearly1$construct_investment1)
+        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm4,df_yearly1$construct_investment1)
       }
       else
       {
         
-        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm1,df_yearly1$svmRegPred)
+        mileage_p<-plotCurve(df_yearly1,df_yearly1$tm4,df_yearly1$svmRegPred)
         
       }
     }
     else{
-      df_yearly1sub<-subset(df_yearly1,substr(df_yearly1$tm1,1,4)>=input$mileage_year_start) 
-      df_yearly1sub<-subset(df_yearly1sub,substr(df_yearly1sub$tm1,1,4)<=input$mileage_year_end)
+      df_yearly1sub<-subset(df_yearly1,substr(df_yearly1$tm4,1,4)>=input$mileage_year_start) 
+      df_yearly1sub<-subset(df_yearly1sub,substr(df_yearly1sub$tm4,1,4)<=input$mileage_year_end)
       if (input$mileage_stat_data) {
-        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm1,df_yearly1sub$construct_investment1)
+        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm4,df_yearly1sub$construct_investment1)
       }
       else
       {
-        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm1,df_yearly1sub$svmRegPred)
+        mileage_p<-plotCurve(df_yearly1sub,df_yearly1sub$tm4,df_yearly1sub$svmRegPred)
       }
     }
     if(input$mileage_predict_data){
-      mileage_p<-mileage_p+geom_line(aes(x=tm1,y=svmRegPred),color="blue",size=0.8)+geom_point(aes(x=tm1,y=svmRegPred),fill='cornsilk',size=4,shape=21,colour="darkblue",position=position_dodge(width=0.2))
+      mileage_p<-mileage_p+geom_line(aes(x=tm4,y=svmRegPred),color="blue",size=0.8)+geom_point(aes(x=tm4,y=svmRegPred),fill='cornsilk',size=4,shape=21,colour="darkblue",position=position_dodge(width=0.2))
       
     }
     
     if (input$mileage_stat_data) {
-      mileage_p<-mileage_p+geom_point(aes(x=tm1,y=construct_investment1),color="red",size=3,shape=21)
+      mileage_p<-mileage_p+geom_point(aes(x=tm4,y=construct_investment1),color="red",size=3,shape=21)
     }
     mileage_p+ylab("固定资产值")+xlab("时间")+geom_point(shape=21,color='red',fill='cornsilk',size=3)
   })
@@ -1440,8 +1440,8 @@ shinyServer(function(input, output) {
   linearRegPred<-df_yearly1$linearRegPred
   frRegPred<-df_yearly1$frRegPred
   svmRegPred<-df_yearly1$svmRegPred
-  tm1<-unique(substr(df_yearly1$tm1,1,4))
-  mileage1_data<-data.frame(tm1,construct_investment1,mileage_delta,linearRegPred,frRegPred,svmRegPred)
+  tm4<-unique(substr(df_yearly1$tm4,1,4))
+  mileage1_data<-data.frame(tm4,construct_investment1,mileage_delta,linearRegPred,frRegPred,svmRegPred)
   
   output$mileage_table<-DT::renderDataTable(
     DT::datatable(
